da2a4666ef1b631c9adbbd92702a4bf3368c8f0a
pangeransaddamhusain <shusainrastafara25@gmail.com>
2023-02-02 21:23:10 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function on() {
+  var menuBar = document.getElementById('menuBar');
+
+  menuBar.classList.toggle('menuBarStyle');
+}
+
+function categoryOn() {
+  var category = document.getElementById('category');
+
+  category.classList.toggle('categoryStyle');
+}
